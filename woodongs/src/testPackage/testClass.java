a3fb111cb469743e700bf45ba34ca91a5017b52f
user <user@192.168.36.105>
2023-07-25 19:19:14 +0900

    ssdasda

--- woodongs/src/testPackage/testClass.java
+++ woodongs/src/testPackage/testClass.java
@@ -2,19 +2,12 @@ package testPackage;
 
 public class testClass {
 	public static void main(String[] args) {
+		System.out.println("safjksdfsdjhfkjdshfjshdf");
 		System.out.println("Hi");
 		System.out.println("Hi");
 		System.out.println("Hi");
-		
 		System.out.println("Hi");
-		System.out.println("Hi");
-		
-		
-		System.out.println("Hi");
-		
-		System.out.println("Hi");
-		System.out.println("Hi");
-		
+	
 		System.out.println("Hi");
 		System.out.println("Hi");
 	}
