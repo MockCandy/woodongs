09937c25326187d0f51ac76185d366269b3fd175
woosik <woosick9292@gamil.com>
2023-07-25 18:41:13 +0900

    hi

---
+++ woodongs/src/testPackage/testClass.java
@@ -0,0 +1,14 @@
+package testPackage;
+
+public class testClass {
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		System.out.println("Hi");
+		System.out.println("Hi");
+		System.out.println("Hi");
+		System.out.println("Hi");
+		System.out.println("Hi");
+		System.out.println("Hi");
+	}
+
+}
